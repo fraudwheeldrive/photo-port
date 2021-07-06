886ee251b6dd6ae64465533138b73087285089e2
[Andrew Core] <[adcore89@gmail.com]>
2021-07-06 19:27:11 -0400

    starting Gallery component

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Gallery() {
+    return (
+        <section>
+        <h1>Name</h1>
+        <p>DESCRIPTION</p>
+        </section>
+    );
+}
+
+export default Gallery;
